ad64083e04510c70863ffb9fecdc522b012fc1de
Madhav-77 <47825436+Madhav-77@users.noreply.github.com>
2020-09-22 16:29:50 +0530

    db config

--- db_config.py
+++ db_config.py
@@ -3,10 +3,16 @@ from flaskext.mysql import MySQL
 
 mysql = MySQL()
 
-# MySQL configurations
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = ''
-app.config['MYSQL_DATABASE_DB'] = 'shop_bridge'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+# local MySQL configurations 
+# app.config['MYSQL_DATABASE_USER'] = 'root'
+# app.config['MYSQL_DATABASE_PASSWORD'] = ''
+# app.config['MYSQL_DATABASE_DB'] = 'shop_bridge'
+# app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+
+# prod MySQL configurations 
+app.config['MYSQL_DATABASE_USER'] = 'sql12366947'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'GGX6Kf1rwz'
+app.config['MYSQL_DATABASE_DB'] = 'sql12366947'
+app.config['MYSQL_DATABASE_HOST'] = 'sql12.freemysqlhosting.net'
 
 mysql.init_app(app)
